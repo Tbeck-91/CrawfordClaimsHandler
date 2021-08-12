08eaa9f0a7a1e98428f5fc9d804554815401b928
Taylor <tbeck6521@conestogac.on.ca>
2021-08-12 04:52:38 -0400

    Add DBContext to services

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+using ClaimsHandler.DataContext;
 
 namespace ClaimsHandler
 {
@@ -24,6 +22,9 @@ namespace ClaimsHandler
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDbContext<InterviewContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("InterviewContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
